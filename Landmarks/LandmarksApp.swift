49ea6f2d37af7b7d7a0cf9d486fc5a96312fbd7d
candyap <candyap182@icloud.com>
2024-08-01 19:44:30 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Candy Pangestu on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
